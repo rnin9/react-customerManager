42ab849b6a9a73d39e3553a130019081917e17dd
rnin9 <rkdalswn1209@kw.ac.kr>
2020-04-19 00:53:29 +0900

    ERR/Access Deny Error

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,33 +7,25 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.get('/api/customers',(req,res)=>{
-    res.send([
-        {
-            id: 1,
-            image: 'https://placeimg.com/64/64/any',
-            name: '이시원',
-            birthday: 950526,
-            gender: '여자',
-            job: '군무원'
-          },
-          {
-            id: 2,
-            image: 'https://placeimg.com/64/64/1',
-            name: '강민주',
-            birthday: 950526,
-            gender: '남자',
-            job: '대학생'
-          },
-          {
-            id: 3,
-            image: 'https://placeimg.com/64/64/2',
-            name: '강민지',
-            birthday: 950526,
-            gender: '여자',
-            job: '선생님'
-          }
+const data = fs.readFileSync('./database.json');
+const conf = JSON.parse(data);
+const mysql = require('mysql');
 
-    ])
-})
+const connection = mysql.createConnection({
+    host: conf.host,
+    user: conf.user,
+    password: conf.port,
+    database: conf.database
+});
+
+connection.connect();
+
+app.get('/api/customers',(req,res)=>{
+    connection.query(
+        "SELECT * FROM CUSTOMER",
+        (err, rows, fields) =>{
+            res.send(rows);
+        }
+    );
+});
 app.listen(port, ()=> console.log(`Listening on port" ${port}`));
